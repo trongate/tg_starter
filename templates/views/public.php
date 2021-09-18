bbd3680a79f641d342d9bb75d54b2e6873c0a1e6
David Connelly <david.webguy@gmail.com>
2021-09-18 18:01:05 +0100

    don't give up your empathy

--- templates/views/public.php
+++ templates/views/public.php
@@ -6,6 +6,8 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?= BASE_URL ?>css/trongate.css">
 	<link rel="stylesheet" href="<?= BASE_URL ?>css/app.css">
+    <!-- don't change anything above here -->
+    <!-- add your own stylesheet below here -->
 	<title>Public</title>
 </head>
 <body>
